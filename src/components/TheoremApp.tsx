5e647498d7fd34659bb48d0c55c58d29ad9f4b81
dev.dmtrllv <34634620+dev-dmtrllv@users.noreply.github.com>
2020-11-24 18:38:50 +0100

    show correct part of app

--- src/components/TheoremApp.tsx
+++ src/components/TheoremApp.tsx
@@ -1,17 +1,42 @@
+import { THEOREM_MAX_PAGES } from "data/theorem";
 import React from "react";
 import { View } from "views";
 import { TheoremContainer } from "./TheoremContainer";
+import { TheoremContext } from "./TheoremContext";
+import { TheoremIntro } from "./TheoremIntro";
 import { TheoremNextButton } from "./TheoremNextButton";
+import { TheoremResults } from "./TheoremResults";
 
 export const TheoremApp: React.FC = () => 
 {
-	return (
-		<View className="theorem-app">
-			<TheoremContainer target="A" />
-			<br />
-			<TheoremContainer target="B" />
-			<br />
-			<TheoremNextButton />
-		</View>
-	);
+	const { theoremPageID } = React.useContext(TheoremContext);
+	console.log(theoremPageID)
+	if(theoremPageID === 0) // app was never loaded before
+	{
+		return (
+			<View>
+				<TheoremIntro />
+			</View>
+		)
+	}
+	else if(theoremPageID > THEOREM_MAX_PAGES) // app was finished
+	{
+		return (
+			<View>
+				<TheoremResults />
+			</View>
+		)
+	}
+	else
+	{
+		return (
+			<View className="theorem-app">
+				<TheoremContainer target="A" />
+				<br />
+				<TheoremContainer target="B" />
+				<br />
+				<TheoremNextButton />
+			</View>
+		);
+	}
 }
