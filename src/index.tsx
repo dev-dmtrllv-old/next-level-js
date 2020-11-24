c71c0fd45670dfca3bd494d96665575065f22fe4
dev.dmtrllv <34634620+dev-dmtrllv@users.noreply.github.com>
2020-11-24 09:38:09 +0100

    added comments and changed app import to reflect file change

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import App from "app";
+import App from "App";
 import React from "react";
 import ReactDOM from "react-dom";
 
@@ -6,6 +6,7 @@ import "./index.scss";
 
 let targetEl = document.getElementById("root");
 
+// create the root element if it does not exists
 if(!targetEl)
 {
 	targetEl = document.createElement("div");
@@ -16,4 +17,5 @@ if(!targetEl)
 // hydrate the app if there are already child elements
 const method = targetEl.children.length > 0 ? "hydrate" : "render";
 
+// hydrate or render the app
 ReactDOM[method](<App />, targetEl);
