1c4c6dd254524505b4b892ba818e5c491a09216a
dev.dmtrllv <34634620+dev-dmtrllv@users.noreply.github.com>
2020-11-24 13:04:54 +0100

    code formatting

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
-import App from "App";
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App";
 
 import "./index.scss";
 
 let targetEl = document.getElementById("root");
 
 // create the root element if it does not exists
-if(!targetEl)
+if (!targetEl)
 {
 	targetEl = document.createElement("div");
 	document.body.appendChild(targetEl);
