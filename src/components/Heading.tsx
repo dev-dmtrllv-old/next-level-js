6144114df29695b3888d9a1f958a65fb086c093a
dev.dmtrllv <34634620+dev-dmtrllv@users.noreply.github.com>
2020-11-25 15:17:07 +0100

    changed size to type (now heading only handles h1 and h2)

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -3,8 +3,8 @@ import { WithReactProps } from "views";
 
 import "./styles/heading.scss";
 
-export const Heading: React.FC<WithReactProps<HeadingProps, HTMLHeadingElement>> = ({ size, ...props }) => React.createElement(`h${size}`, { ...props });
+export const Heading: React.FC<WithReactProps<HeadingProps, HTMLHeadingElement>> = ({ type = "header", ...props }) => React.createElement(type === "header" ? "h1" : "h2", { ...props });
 
 type HeadingProps = {
-	size: number;
+	type?: "header" | "sub";
 };
