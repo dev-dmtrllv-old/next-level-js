305ab6572c9bca0d6c5c02ed721ea40edc1b989a
dev.dmtrllv <34634620+dev-dmtrllv@users.noreply.github.com>
2020-12-01 12:47:30 +0100

    added retry link for testing

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,17 +2,21 @@ import React from "react";
 import { View } from "views";
 import { Container } from "./Container";
 import { Logo } from "./Logo";
+import { TheoremContext } from "./TheoremContext";
 
 import "./styles/header.scss";
 
 export const Header = () =>
 {
+	const { retry } = React.useContext(TheoremContext);
+
 	return (
 		<View id="header-wrapper">
 			<View id="header" type="header" position="fixed">
 				<Container fill>
 					<Logo />
 				</Container>
+				<a onClick={retry} style={{ position: "fixed", right: 0, top: 0, padding: "15px" }}>reset</a>
 			</View>
 		</View>
 	);
