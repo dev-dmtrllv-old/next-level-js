986100cde3ba359f212648f2628e0018d5064b23
dmtrllv <dev.dmtrllv@gmail.com>
2021-01-17 17:53:45 +0100

    added content

--- src/components/TimeUpModal.tsx
+++ src/components/TimeUpModal.tsx
@@ -1,13 +1,53 @@
 import React from "react";
+import { FlexBox, FlexItem, View } from "views";
+
 import { TheoremContext } from "./TheoremContext";
+import { Container } from "./Container";
+import { Heading } from "./Heading";
+import { Panel } from "./Panel";
+import { Button } from "./Button";
+
+import "./styles/time-up-modal.scss";
 
 export const TimeUpModal = () =>
 {
-	const { isTimeUp } = React.useContext(TheoremContext);
+	const { isTimeUp, toResults } = React.useContext(TheoremContext);
 
-	if(isTimeUp)
+	if (isTimeUp)
 		return (
-			<h1>Time Is Up BOII!!</h1>
+			<View id="time-up-modal" position="absolute" fill>
+				<Container fill>
+					<View className="modal" position="absolute" center>
+						{/* <View fill> */}
+							<Panel>
+								<FlexBox dir="vertical">
+									<FlexItem base={64}>
+										<View position="absolute" center="vertical">
+											<Heading type="header">
+												De tijd is verlopen!
+											</Heading>
+										</View>
+									</FlexItem>
+									<FlexItem className="body">
+										<p>
+											De tijd is verlopen, dit betekent gelukkig niet dat er geen resultaat is!
+											<br />
+											Klik hieronder op de knop om naar het resultaat te gaan.
+										</p>
+									</FlexItem>
+									<FlexItem base={64}>
+										<View position="absolute" center>
+											<Button type="tertiary" onClick={toResults}>
+												Resultaat
+										</Button>
+										</View>
+									</FlexItem>
+								</FlexBox>
+							</Panel>
+						{/* </View> */}
+					</View>
+				</Container>
+			</View>
 		);
 	return null;
 };
