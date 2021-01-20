16a8305bc27271954627c758723dac6ddfad6767
dmtrllv <dev.dmtrllv@gmail.com>
2021-01-20 19:20:34 +0100

    added timer notification styling

--- src/components/TimerNotification.tsx
+++ src/components/TimerNotification.tsx
@@ -1,17 +1,27 @@
 import React from "react";
+import { Container, Heading, View } from "views";
 
 import { TheoremContext } from "./TheoremContext";
+import { Panel } from "./Panel";
+
+import "./styles/timer-notification.scss";
 
 export const TimerNotification = () =>
 {
 	const { showLeftTime, hideTimerNotification } = React.useContext(TheoremContext);
 
-	if (showLeftTime)
-		return (
-			<div>
-				Let op! Je hebt nog 5 minuten.
-				<button onClick={hideTimerNotification}>close</button>
-			</div>
-		);
-	return null;
+	return (
+		<View id="timer-notification" className={showLeftTime ? "show" : ""} position="absolute">
+			<Container>
+				<Panel className="inner-notification">
+					<View position="absolute" center="vertical">
+						<Heading type="sub">Let op! U heeft nog 5 minuten.</Heading>
+					</View>
+					<View className="btn-close" center="vertical" position="absolute" onClick={hideTimerNotification}>
+						<View className="inner-btn" position="absolute" center />
+					</View>
+				</Panel>
+			</Container>
+		</View>
+	);
 };
