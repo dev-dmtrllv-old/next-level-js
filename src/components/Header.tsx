0489931e29ed88c1536939c22f8e84d07ad3a4a9
dmtrllv <dev.dmtrllv@gmail.com>
2021-06-21 11:21:58 +0200

    hopefully last update :P

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,17 +3,21 @@ import React from "react";
 import { View, Container } from "views";
 
 import { Logo } from "./Logo";
+import { TheoremContext } from "./TheoremContext";
 
 import "./styles/header.scss";
 
 export const Header = () =>
 {
+	const { retry } = React.useContext(TheoremContext);
+
 	return (
 		<View id="header-wrapper">
 			<View id="header" type="header" position="fixed">
 				<Container fill>
 					<Logo />
 				</Container>
+				<button style={{ position: "absolute", right: 25, top: "50%", transform: "translate(0%, -50%)" }} onClick={() => retry()}>reset</button>
 			</View>
 		</View>
 	);
